75fcb688c340022753e0085d5982f0981fea142e
Mduduzi-Ndlovu1 <Mduduzindlovu02@gmail.com>
2024-05-30 13:34:01 +0200

    top destinaiton done

--- js/swiper.js
+++ js/swiper.js
@@ -19,3 +19,31 @@ const swiper = new Swiper('.swiper', {
     speed:400,
     spaceBetween: 100,
   });
+
+  const swiper2 = new Swiper('.swiper2', {
+    slidesPerView: 3,
+    spaceBetween: 35,
+    slidesPerGroup:1,
+    loop: true,
+    fade:true,
+    centerSlide:true,
+    grabCursor:true,
+    loopFillGroupWithBlank: true,  
+    autoplay: {
+        delay: 5000,
+    },
+
+    speed:400,
+
+    breakpoints: {
+        320: {
+            slidesPerView:1,
+        },
+        768: {
+            slidesPerView:2,
+        },
+        968: {
+            slidesPerView:3,
+        },
+    },
+  });
